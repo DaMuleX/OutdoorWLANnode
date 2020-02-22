43d4f78b95a460f0b3698f9a4a4bd2cddf63b4a8
Clemens Briese <clemens.briese@gmail.com>
2020-02-22 09:47:27 +0100

    LED blinkt

---
+++ Node_WLAN_ESP32MCU/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define LED_BUILTIN 15
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(5000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Blink");
+   // wait for a second
+  delay(5000);
+}
